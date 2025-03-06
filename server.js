ec3f9f0660727ae13210842e2c02dddcb7714efc
shrutinimai <nimaishruti@gmail.com>
2025-03-06 23:37:20 +0530

    integrating and adding extra content in html

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const userRoutes = require("./routes/users");
 const productRoutes = require("./routes/products");
 const cartRoutes = require("./routes/cart");
 
-app.use(express.json());  
+
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
+
+app.get("/api/products", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 app.get("/", (req, res) => {
     res.send("Welcome to the E-Commerce API!");
 });
 
-app.use((req, res) => {
-    res.status(404).send("Page not found");
-});
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
