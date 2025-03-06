1db642eb2fd3b8562142de033171e682a6f42e63
shrutinimai <nimaishruti@gmail.com>
2025-03-06 17:59:51 +0530

    updated product routes and controllers

---
+++ controllers/productController.js
@@ -0,0 +1,11 @@
+exports.getAllProducts = (req, res) => {
+    res.send("Fetching all products");
+};
+
+exports.getProductById = (req, res) => {
+    res.send(`Fetching product with ID: ${req.params.id}`);
+};
+
+exports.addProduct = (req, res) => {
+    res.send("Adding a new product");
+};
--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const productController = require("../controllers/productController");
 
-router.get("/", (req, res) => {
-    res.send("Fetching all products");
-});
-
-router.post("/", (req, res) => {
-    res.send("Adding a new product");
-});
-
-router.get("/:id", (req, res) => {
-    res.send(`Fetching product with ID: ${req.params.id}`);
-});
+router.get("/", productController.getAllProducts);
+router.post("/", productController.addProduct);
+router.get("/:id", productController.getProductById);
 
 module.exports = router;
